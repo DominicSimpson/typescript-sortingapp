ef2408a114a95010a3a824ceb32c2f36af2cfec1
db_simpson@yahoo.co.uk <db_simpson@yahoo.co.uk>
2025-11-15 11:49:03 +0000

    Refactoring abstract class

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,22 @@ import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
-
+import { link } from 'fs';
+ 
 
 // const numbersCollection = new NumbersCollection([50, 3, -5, 0]);
 // numbersCollection.sort();
 // console.log(numbersCollection.data);
 
-const charactersCollection = new CharactersCollection('Xaayb');
-charactersCollection.sort();
-console.log(charactersCollection.data);
+// const charactersCollection = new CharactersCollection('Xaayb');
+// charactersCollection.sort();
+// console.log(charactersCollection.data);
 
-// const linkedList = new LinkedList();
-// linkedList.add(500);
-// linkedList.add(-10);
-// linkedList.add(-3);
-// linkedList.add(4);
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
 
-// const sorter = new Sorter(linkedList);
-// sorter.sort();
-// linkedList.print();
+linkedList.sort()
+linkedList.print();
